ea3dd205b7021b4eb9b9e7f991e563faf710c96c
pawanyogi <yogipawanofficial01@gmail.com>
2022-12-03 20:55:19 +0530

    demo product added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import Header from './components/Header';
+import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <main>
-      <Container>
-     <h1>this is natureFresh</h1>
-     </Container>
-     </main>
-     <Footer/>
+        <Container>
+          <h1>this is natureFresh</h1>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
