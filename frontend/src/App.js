70d077c5e8283870ecf9f839958131e3a6df8b42
pawanyogi <yogipawanofficial01@gmail.com>
2022-12-04 01:07:55 +0530

    react-router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <Routes>
-      <div>
-        <Header />
-        <main className="my-3">
-          <Container>
-            <h1>this is natureFresh</h1>
+    <div>
+      <Header />
+      <main className="my-3">
+        <Container>
+          <h1>this is natureFresh</h1>
+          <Routes>
             <Route path="/" element={<HomeScreen />} />
-            <HomeScreen />
-          </Container>
-        </main>
-        <Footer />
-      </div>
-    </Routes>
+            <Route path="/product/:product_id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
