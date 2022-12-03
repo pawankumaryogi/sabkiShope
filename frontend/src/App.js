c31ab6f305464fb41b3f05964abb23aec6a3207a
pawanyogi <yogipawanofficial01@gmail.com>
2022-12-04 00:41:57 +0530

    Rating added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <Container>
-          <h1>this is natureFresh</h1>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <Routes>
+      <div>
+        <Header />
+        <main className="my-3">
+          <Container>
+            <h1>this is natureFresh</h1>
+            <Route path="/" element={<HomeScreen />} />
+            <HomeScreen />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Routes>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-
+import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
@@ -9,13 +9,13 @@ const Product = ({ product }) => {
       </a>
       <Card.Body>
         <a href={`/product/${product._id}`}>
-          <Card.Title>
-            <strong>{product.name}</strong>{" "}
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
           </Card.Title>
         </a>
         <Card.Text as = 'div'>
             <div className="my-3">
-                    {product.rating} from {product.numReviews}
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             </div>
 
         </Card.Text>
