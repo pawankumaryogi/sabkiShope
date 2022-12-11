20f30e5d765cf6b97155ea1101f5148072f9c465
pawanyogi <yogipawanofficial01@gmail.com>
2022-12-12 02:12:42 +0530

    custom_auth middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,20 +1,3 @@
-// const notFound = (req, res, next) => {
-//   const errorr = new Error(`Not Found-${req.originalUrl}`);
-//   res.status(404);
-//   next(errorr);
-// };
-
-// const errorHandler = (err, req, res, next) => {
-//   const statusCode = res.statusCode === 200 ? 500 : statusCode;
-//   res.status(statusCode);
-//   res.json({
-//     message: err.message,
-//     stack: process.NODE_ENV === "production" ? null : err.stack,
-//   });
-// };
-
-// export { notFound, errorHandler };
-
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
